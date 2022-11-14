9c7ab550bbd8739f9bde7ce0efc018186355ecbb
Zheng Yune <yunethecoder@gmail.com>
2022-11-13 21:12:27 -0500

    update

--- hw4/feature_1.py
+++ hw4/feature_1.py
@@ -3,27 +3,12 @@ from DecisionTreeClassifier import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
 import math
-# df = pd.read_csv("PS4-1/PS4_1_X_Train.csv")
-# y_train = pd.read_csv("PS4-1/PS4_2_Y_Train.csv")
-# df['target'] = y_train['0']
-# df = df.sort_values(by="-0.21961")
-# X = df['-0.21961'].values
-# X = X.reshape(-1, 1)
-# y = df['target'].values
-# y = y.reshape(-1, 1)
-# num_of_test_data = len(X)
-# gap = math.ceil(num_of_test_data/20)
-# X = X[::gap]
-# y = y[::gap]
 df = pd.read_csv("PS4-1/PS4_1_X_Train.csv")
 X = df.values
 y_train = pd.read_csv("PS4-1/PS4_2_Y_Train.csv")
 y = y_train.values
 len(y)
-# num_of_test_data = len(X)
-# gap = math.ceil(num_of_test_data/20)
-# X = X[::gap]
-# y = y[::gap]
+
 classifier = DecisionTreeClassifier(min_samples_split=2, max_depth=6)
 classifier.fit(X, y)
 classifier.print_tree()
--- hw4/feature_2.py
+++ hw4/feature_2.py
@@ -1,19 +1,24 @@
+from sklearn.metrics import accuracy_score
 from DecisionTreeClassifier import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
 import math
+import random
 df = pd.read_csv("PS4-1/PS4_1_X_Train.csv")
+X = df.values
 y_train = pd.read_csv("PS4-1/PS4_2_Y_Train.csv")
-df['target'] = y_train['0']
-df = df.sort_values(by="2.0415")
-X = df['2.0415'].values
-X = X.reshape(-1, 1)
-y = df['target'].values
-y = y.reshape(-1, 1)
-num_of_test_data = len(X)
-gap = math.ceil(num_of_test_data/20)
-X = X[::gap]
-y = y[::gap]
+y = y_train.values
+number = len(y)
+print(y)
+count = 0
+for i in y:
+    if i == [0]:
+        count += 1
+print(count)
+for i in range(20):
+    ran_num = random.randint(0, count)
+    y[ran_num] = [1]
+
 classifier = DecisionTreeClassifier(min_samples_split=2, max_depth=6)
 classifier.fit(X, y)
 classifier.print_tree()
